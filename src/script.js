819579833f0d1ea0e71b85923c3a010d7a002d91
saurabhkkanaujia <saurabhkumarkanaujia@cedcoss.com>
2022-02-11 09:31:58 +0530

    Full Code Restructured

--- src/script.js
+++ src/script.js
@@ -1,3 +1,31 @@
+
+$(document).ready(function(){
+    display(products);
+
+    $('#os, #brand').on("change", function(){
+        selectedOS = $('#os').find('option:selected').val();
+        selectedBrand = $('#brand').find('option:selected').val();
+        a = filterProducts(selectedOS, selectedBrand);
+        display(a);
+        console.log('aa');
+        display(a);
+    });
+
+    $('body').on("click", "#close", function(){
+        var row_id = $(this).data("row_id");
+        $('#'+row_id).hide();
+    });
+
+    //search bar
+    $("#search").on("keyup", function() {
+		var value = $(this).val().toLowerCase();
+		$("table tr").filter(function() {
+		  $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+		});
+	});
+    
+    
+});
 var products = [{
     "id": "100",
     "name": "iPhone 4S",
@@ -8,7 +36,7 @@ var products = [{
     "id": "101",
     "name": "Moto X",
     "brand": "Motorola",
-    "os": "Android"	
+    "os": "Android"
 },
 {
     "id": "102",
@@ -34,140 +62,98 @@ var products = [{
     "brand": "Microsoft",
     "os": "Windows"
 }];
+
 var html = '';
 var os_array= new Set();
 var brand_array= new Set();
-var displayOS = [];
-var displayBrand = [];
 var prod_to_show = [];
-var osDropdown = '';
-var brandDropdown = '';
-var os_val = 'none1';
-var brand_val = 'none1';
+var selectedBrand = 'none1';
+var selectedOS = 'none1';
 
-$(document).ready(function(){
-    for (var i = 0; i<products.length; i++){
-        os_array.add(products[i].os)
-        brand_array.add(products[i].brand)
+for (var i = 0; i<products.length; i++){
+    os_array.add(products[i].os)
+    brand_array.add(products[i].brand)
+}
+
+
+//filter function
+function filterProducts(selectedOS, selectedBrand){
+    prod_to_show = [];
+    for (i=0; i<products.length; i++){
+        if ((selectedOS == products[i].os) && (selectedBrand == products[i].brand)){
+            prod_to_show.push(products[i]);
+        }
+        else if (selectedOS == 'none1' && selectedBrand == 'none1'){
+            prod_to_show = products;
+        }
+        else if (selectedOS == 'none1' && selectedBrand == products[i].brand){
+            prod_to_show.push(products[i]);
+        }
+        else if (selectedOS == products[i].os && selectedBrand == 'none1'){
+            prod_to_show.push(products[i]);
+        }
+
     }
-        
-    display(products);
+    return prod_to_show;
+}
+
+
+
+//function to render content
+function display(result){
+    html = '<div>';
+    html += dropDown();
+    html += '<input type = "text" id = "search" >';
+
+    html += '<table>\
+    <tr>\
+        <th>ID</th>\
+        <th>Name</th>\
+        <th>Brand</th>\
+        <th>Operating System </th>\
+        <th>Remove</th>\
+    </tr>';
+
+    for (var i=0; i < result.length; i++){
+    html += '<tr id='+result[i].id+'>\
+        <td>'+result[i].id+'</td>\
+        <td>'+result[i].name+'</td>\
+        <td>'+result[i].brand+'</td>\
+        <td>'+result[i].os+'</td>\
+        <td><a href = "#" id = "close" data-row_id='+ result[i].id +' >X</a></td>\
+        </tr>'
+    }
+    // document.getElementById('table').innerHTML = html;
     
+    html+='</table></div>';
 
-    $('body').on("click", "#close", function(){
-        var row_id = $(this).data("row_id");
-        $('#'+row_id).hide();
+    $('#wrapper').html(html);
+    return 
+}
 
-    });
+
+//function for DropDown
+function dropDown(){
     // Dropdown for OS
-    osDropdown = '<label for="os">Filter by OS:</label>\
+    var osDropdown = '<div>\
+                    <label for="os">Filter by OS:</label>\
                     <select name="os" id="os">\
                     <option value = "none1" id = "none">None</option>';
-
     for (var item of os_array){
         osDropdown += '<option value="'+item+'" id = "'+ item +'">'+ item +'</option>';
     }
-
     osDropdown+= '</select>'
-
+    // Dropdown End
     // Dropdown for Brand
-    brandDropdown = '<label for="brand">Filter by Brand:</label>\
+    var brandDropdown = '<label for="brand">Filter by Brand:</label>\
                     <select name="brand" id="brand">\
                     <option value = "none1">None</option>';
-
     for (var item of brand_array){
         brandDropdown += '<option value="'+item+'">'+ item +'</option>';
     }
-
-    brandDropdown+= '</select>'
-    $('table').before(brandDropdown);
+    brandDropdown+= '</select>\
+    </div>';
     // Dropdown End
 
-    $('table').before(osDropdown);
-
-
-    $('#os').change(function(){
-        var filterValOS = $(this).val();
-
-        for (var i=0; i<products.length; i++){
-            if (products[i].os == filterValOS){
-                displayOS.push(products[i]);
-            }
-            else if (filterValOS == 'none1'){
-                displayOS = products;
-            }
-        }
-        display(displayOS, osDropdown, brandDropdown);
-    });
-
-    $('#brand').change(function(){
-        var filterValBrand = $(this).val();
-
-        for (var i=0; i<products.length; i++){
-            if (products[i].brand == filterValBrand){
-                displayBrand.push(products[i]);
-            }
-            else if (filterValBrand == 'none1'){
-                displayBrand = products;
-            }
-        }
-        display(displayBrand, osDropdown, brandDropdown);
-        
-    });
-
-
-    searchBar = '<input type = "text" id = "search" >';
-
-
-
-
-    $('#wrapper').before(searchBar);
-    $("#search").on("keyup", function() {
-		var value = $(this).val().toLowerCase();
-		$("table tr").filter(function() {
-		  $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-		});
-	  });
-    
-
-    // $('#bb').click(function(){
-    //     for (var i = 0; i<displayBrand.length; i++){
-    //         for (var j = 0 ; j<displayOS.length; j++){
-    //             if (displayBrand[i] == displayOS[j]){
-    //                 prod_to_show.push(displayOS[j]);
-    //             }
-    //         }
-    //     }
-    //     console.log(prod_to_show);
-    //     display(prod_to_show);
-    // });
-
-});
-
-
-function display(resultArray, osDropdown, brandDropdown){
-    html = '<table>\
-            <tr>\
-                <th>ID</th>\
-                <th>Name</th>\
-                <th>Brand</th>\
-                <th>Operating System </th>\
-                <th>Remove</th>\
-            </tr>';
-
-    for (var i=0; i < resultArray.length; i++){
-        html += '<tr id='+resultArray[i].id+'>\
-        <td>'+resultArray[i].id+'</td>\
-        <td>'+resultArray[i].name+'</td>\
-        <td>'+resultArray[i].brand+'</td>\
-        <td>'+resultArray[i].os+'</td>\
-        <td><a href = "#" id = "close" data-row_id='+ resultArray[i].id +' >X</a></td>\
-    </tr>'
-    }
-    // document.getElementById('table').innerHTML = html;
-    html+='</table>';
-    $('#wrapper').html(html);
-    $('table').before(osDropdown);
-    $('table').before(brandDropdown);
-
+    return osDropdown+brandDropdown;
 }
